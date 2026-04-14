87c4e5d0377402f420b1302d9cc012a449fc9dd3
kimkihyeon <kimki1125@naver.com>
2026-04-14 14:45:06 +0900

    [feat] AuthPlugin에 커스텀 prefix 지원 추가 (KakaoAK 등)

--- Modules/Core/Network/Sources/AuthPlugin.swift
+++ Modules/Core/Network/Sources/AuthPlugin.swift
@@ -3,15 +3,17 @@ import Moya
 
 public struct AuthPlugin: PluginType {
     private let tokenProvider: @Sendable () -> String?
+    private let prefix: String
 
-    public init(tokenProvider: @escaping @Sendable () -> String?) {
+    public init(prefix: String = "Bearer", tokenProvider: @escaping @Sendable () -> String?) {
+        self.prefix = prefix
         self.tokenProvider = tokenProvider
     }
 
     public func prepare(_ request: URLRequest, target: any TargetType) -> URLRequest {
         guard let token = tokenProvider() else { return request }
         var request = request
-        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        request.addValue("\(prefix) \(token)", forHTTPHeaderField: "Authorization")
         return request
     }
 }
