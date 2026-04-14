e2e69e0be7faccdd74614c612cf93966fb546cf2
kimkihyeon <kimki1125@naver.com>
2026-04-14 10:20:57 +0900

    Initial Commit

---
+++ ForP/ForPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForPApp.swift
+//  ForP
+//
+//  Created by 김견 on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct ForPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
